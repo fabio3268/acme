6cb1226d19da4ee4e7fffe4243c0f89cf9360377
Fábio Santos <fabio3268@gmail.com>
2022-07-06 10:04:42 -0300

    Links Amigáveis

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ ob_start();
 require __DIR__ . "/vendor/autoload.php";
 use CoffeeCode\Router\Router;
 
-//$route = new Router('https://www.localhost/acme', ":");
-$route = new Router('localhost/acme', ":"); // Route para localhost
+$route = new Router('https://www.localhost/acme', ":");
+//$route = new Router('localhost/acme', ":"); // Route para localhost
 
 $route->namespace("Source\App");
 $route->get("/","Web:home");
@@ -17,13 +17,17 @@ $route->get("/patrocinadores","Web:sponsors");
  * Erros Routes
  */
 
-//$route->namespace("Source\App")->group("/ops");
-//$route->get("/{errcode}", "Web:error");
+$route->namespace("Source\App")->group("/ops");
+$route->get("/{errcode}", "Web:error");
 
 $route->dispatch();
 
-//if($route->error()){
-//    $route->redirect("/ops/{$route->error()}");
-//}
+/*
+ * Error Redirect
+ */
+
+if($route->error()){
+    $route->redirect("/ops/{$route->error()}");
+}
 
 ob_end_flush();
